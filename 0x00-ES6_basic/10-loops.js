4b155f4880068863291f770e333594fff26caf07
houssam980 <houssam.ghmari1243@gmail.com>
2024-05-27 22:31:36 +0000

    for and const

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+    const appendedArray = [];
+    for (const idx of array) {
+      const value = idx;
+      appendedArray.push(appendString + value);
+    }
+  
+    return appendedArray;
+  }
