0e0750df4f26e06202302c818138c30f79b66cc8
houssam980 <houssam.ghmari1243@gmail.com>
2024-05-29 15:41:26 +0000

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((resol) => {
+      console.log(`${resol[0].body} ${resol[1].firstName} ${resol[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
