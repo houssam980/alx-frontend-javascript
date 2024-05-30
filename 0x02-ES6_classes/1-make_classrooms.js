27343a0652c1f4cfe75309b957100003623421ca
houssam980 <houssam.ghmari1243@gmail.com>
2024-05-30 16:47:12 +0000

    classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import ClassRoom from './0-ClassRoom';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-
+  
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
