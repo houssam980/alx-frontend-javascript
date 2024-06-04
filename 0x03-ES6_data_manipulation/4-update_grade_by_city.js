acd985bf32b9d9d24b3cffc4c319b0e5e622b7ff
houssam980 <houssam.ghmari1243@gmail.com>
2024-06-04 23:57:08 +0000

    pushit

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(array, city, grad) {
+    return array
+        
+        .filter((item) => item.location === city)
+        .map((student) => {
+            
+            const gradeItems = grad
+                .filter((item) => item.studentId === student.id)
+                .map((n) => n.grd)[0];
+            const grd = gradeItems || 'N/A';
+            return { ...student, grd };
+        });
+}
